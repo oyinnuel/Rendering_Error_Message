5c8b87657108cc02c4ae23704617f83571e1d072
oyinnuel <47600584+oyinnuel@users.noreply.github.com>
2022-06-30 13:22:49 +0100

    Initializing files for rendering error message

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+function purchase() {
+    // When the user clicks the purchase button, render out
+    error = document.getElementById("error")
+    // "Something went wrong, please try again" in the paragraph
+    error.textContent = "Something went wrong, please try again"
+    // that has the id="error".
+    
+}
